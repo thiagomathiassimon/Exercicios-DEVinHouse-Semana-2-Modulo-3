21f2993df73868021d291d4bb293af01b771ad52
thiagomathiassimon <mathiastsimon@gmail.com>
2021-06-26 13:24:03 -0300

    Exercicio-1

---
+++ seguranca-frontend/src/App.js
@@ -0,0 +1,22 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div id="title">
+        <h1>
+          Bem vindo aos nossos exercicios sobre Autorização e Autenticação de
+          uma aplicação Client Frontend
+        </h1>
+      </div>
+      <div id="content">
+        <button id="logar">Fazer Login</button>
+        <button id="api">Chamar API</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
