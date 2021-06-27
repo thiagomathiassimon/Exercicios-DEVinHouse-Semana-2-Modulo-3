99c026aefb36faf1814dce0c11347935a81e8cce
thiagomathiassimon <mathiastsimon@gmail.com>
2021-06-27 09:26:55 -0300

    Exercicio-17

--- seguranca-frontend/src/App.js
+++ seguranca-frontend/src/App.js
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import { useHistory } from "react-router-dom";
 import "./App.css";
 import "./styles.css";
 import UserService from "./services/UserService";
+import axios from "axios";
 
 function App() {
+  const history = useHistory();
 
-const history = useHistory();
+  const [message, setMessage] = useState();
+
+  const handleChamarAPI = () => {
+    axios
+      .get("http://localhost:8080/exercicios")
+      .then((response) => setMessage(response.data));
+  };
 
   return (
     <div className="App">
@@ -16,11 +25,20 @@ const history = useHistory();
         </h1>
       </div>
       <div id="content">
-        <button id="logar" onClick={() => {
-          UserService.doLogout();
-          history.push("/login")}}>Fazer Login</button>
-        <button id="api">Chamar API</button>
+        <button
+          id="logar"
+          onClick={() => {
+            UserService.doLogout();
+            history.push("/login");
+          }}
+        >
+          Fazer Login
+        </button>
+        <button id="api" onClick={handleChamarAPI}>
+          Chamar API
+        </button>
       </div>
+      <div id="message"><h2>{message}</h2></div>
     </div>
   );
 }
